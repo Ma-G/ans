f1be1c578b0a583be85e7b7c62b5f0768abea3cb
Ma-G <magda0gie@gmail.com>
2020-03-26 15:53:47 +0100

    bash script for installing and initializing vagrant virtual machine

--- install_vagrant.sh
+++ install_vagrant.sh
@@ -1,3 +1,15 @@
-#!/bin/bash
+#!/bin/bash -xe
+#install vagrant and initialize ubuntu with vagrant and virtualbox
 
+DIRECTORY=./vagrant/ubuntu
+if [ -d "$DIRECTORY" ]; then
+	mkdir -p vagrant/ubuntu
+fi
 
+cd vagrant/ubuntu
+
+sudo dnf install vagrant 
+
+vagrant init ubuntu/trusty64
+
+vagrant up --provider virtualbox
